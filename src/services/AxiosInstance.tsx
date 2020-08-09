79c0a1b985bf9e9a77c97e222b55d5f79b5d43b9
Motti Friedman <Motti.Friedman@gmail.com>
2020-08-09 18:10:29 +0300

    fixed consistent folder mapping between dev and production

--- src/services/AxiosInstance.tsx
+++ src/services/AxiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 // import * as msalInstance from './msalInstance';
 
-const baseURL = process.env.NODE_ENV === "development" ? "http://localhost:5000/" : "/backend/";
+const baseURL = process.env.NODE_ENV === "development" ? "http://localhost:5000/backend/" : "/backend/";
 
 const axiosConfig = {
   baseURL,
